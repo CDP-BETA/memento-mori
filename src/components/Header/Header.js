0bd0e5bb9b1fc2cd410c030136dad00def65f500
zerone2 <yicho93@naver.com>
2020-06-19 14:55:55 +0900

    [add: Header]

---
+++ src/components/Header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './globalHeader.scss'
+
+const Header = () => {
+  return (
+    <header className={'header'}>
+      <p>
+        <Link to={'/'}>HOME</Link>
+      </p>
+      <p>
+        <Link to={'/test'}>TEST</Link>
+      </p>
+      <p>
+        <Link to={'/login'}>LOGIN</Link>
+      </p>
+    </header>
+  )
+}
+
+export default Header
