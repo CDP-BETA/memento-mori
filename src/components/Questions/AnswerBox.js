6641ac2c8395b0662701ba85b97c73de78748e05
zerone2 <yicho93@naver.com>
2020-07-01 00:17:48 +0900

    [fix: question page style]

--- src/components/Questions/AnswerBox.js
+++ src/components/Questions/AnswerBox.js
@@ -13,13 +13,17 @@ const AnswerBox = observer(() => {
     stepNext()
   }
 
+  const getHeight = () => {
+    if(window.innerHeight < 600) return { height: '7rem' }
+    else if(window.innerHeight > 700) return { height: '9.5rem'}
+  }
   return (
     <div className={'answerBoxContainer'}>
-      <div className={'answerBox'} onClick={() => setAnswer(0)}>
+      <div className={'answerBox'} onClick={() => setAnswer(0)}  style={getHeight()}>
         <img src={answerIcons[0]} alt={'answer_icon1'} />
         <p>{stepAnswers[0]}</p>
       </div>
-      <div className={'answerBox'} onClick={() => setAnswer(1)}>
+      <div className={'answerBox'} onClick={() => setAnswer(1)}  style={getHeight()}>
         <img src={answerIcons[1]} alt={'answer_icon2'} />
         <p>{stepAnswers[1]}</p>
       </div>
