1a4f30a70b747aec5f66cf0801b7a17c40ac4ae2
zerone2 <yicho93@naver.com>
2020-07-08 10:52:21 +0900

    [fix: App.js]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { create } from 'mobx-persist'
-import { Provider, observer, useLocalStore } from 'mobx-react'
+import { Provider, observer } from 'mobx-react'
 import { Router, Route, Switch } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
 import { stores } from 'stores'
@@ -8,29 +8,24 @@ import { stores } from 'stores'
 import { HomePage, QuestionPage, ResultPage } from './pages'
 
 const browserHistory = createBrowserHistory()
-const hydrate = create()
 
 const App = observer(() => {
-  const store = useLocalStore(() => ({
-    storeLoaded: false,
-    setStoreLoaded: (load) => (store.storeLoaded = load),
-  }))
+  const [storeLoaded, setStoreLoaded] = useState(false)
 
-  React.useEffect(() => {
+  useEffect(() => {
+    const hydrate = create()
+    hydrate('userStore', stores.userStore).then(() => {
+      setStoreLoaded(true)
+    })
+
     window.Kakao.init(process.env.REACT_APP_KAKAO_KEY)
     console.log(Kakao.isInitialized() && 'kakao initialized.')
-    const load = async () => {
-      await hydrate('userStore', stores.userStore).then(() => {
-        store.setStoreLoaded(true)
-      })
-    }
-    load()
   }, [])
 
   return (
     <Provider {...stores}>
       <Router history={browserHistory}>
-        {store.storeLoaded ? (
+        {storeLoaded ? (
           <Switch>
             <Route exact path={'/'} component={HomePage} />
             <Route path={'/question'} component={QuestionPage} />
